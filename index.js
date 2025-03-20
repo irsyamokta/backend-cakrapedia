08bd469cd97b729fedd411d753884cb987d5ee01
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-20 19:20:13 +0700

    Update : Index

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const newsRoutes = require("./src/routes/newsRoutes");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./src/routes/authRoutes.js";
 
+dotenv.config();
 const app = express();
 
-app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"));
-
-app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "Welcome to the API",
-    });
-})
+app.use(express.json());
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
