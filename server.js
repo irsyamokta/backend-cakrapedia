76576f704dde7488813462fb12a05d8d7187ff87
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-22 23:34:29 +0700

    Update : Server

---
+++ server.js
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import xssClean from "xss-clean";
+import hpp from "hpp";
+import rateLimit from "express-rate-limit";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+import AppError from "./src/utils/AppError.js";
+import authRoutes from "./src/routes/authRoutes.js";
+import userRoutes from "./src/routes/userRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100,
+    message: "Too many requests from this IP, please try again after 15 minutes",
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+app.use(limiter);
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5000",
+    credentials: true
+}));
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(xssClean());
+app.use(hpp());
+app.use(cookieParser());
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
+
+app.get("/", (req, res) => res.send("Server is running"));
+
+app.use('*', (req, res, next) => {
+    next(new AppError(404, "fail", "Undefined route"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
