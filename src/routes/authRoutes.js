c3f3a3f8371dfea0fa5f0c431038b0cf6e57cd20
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-23 22:04:04 +0700

    Update : Auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,18 @@
 import express from "express";
-import { register, login, logout, refreshToken } from "../controllers/authController.js";
+import { register, login, logout, refreshToken, verifyEmail } from "../controllers/authController.js";
+import { changePassword, forgotPassword, resetPassword } from "../controllers/passwordController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/refresh", refreshToken);
 router.post("/register", register);
+router.get("/verify/:token", verifyEmail);
 router.post("/login", login);
+router.post("/change-password", authMiddleware, changePassword);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+router.get("/reset-password/:token", resetPassword);
 router.post("/logout", authMiddleware, logout);
 
 export default router;
