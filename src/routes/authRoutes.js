54383e25cee0c00897286fb3df13737adc24693d
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-24 23:00:41 +0700

    Add : Middleware

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { register, login, logout, refreshToken, verifyEmail } from "../controllers/authController.js";
 import { changePassword, forgotPassword, resetPassword } from "../controllers/passwordController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { checkVerifiedUser } from "../middlewares/checkVerifiedUser.js";
 
 const router = express.Router();
 
@@ -9,10 +10,10 @@ router.post("/refresh", refreshToken);
 router.post("/register", register);
 router.get("/verify/:token", verifyEmail);
 router.post("/login", login);
-router.post("/change-password", authMiddleware, changePassword);
+router.post("/change-password", authMiddleware, checkVerifiedUser,changePassword);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 router.get("/reset-password/:token", resetPassword);
-router.post("/logout", authMiddleware, logout);
+router.post("/logout", authMiddleware, checkVerifiedUser, logout);
 
 export default router;
