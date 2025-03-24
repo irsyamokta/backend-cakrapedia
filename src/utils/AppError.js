bf2c9c715aa045a0d13898935559ffa4ff5e5ebe
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-25 02:28:02 +0700

    Del : Unused file

--- src/utils/AppError.js
+++
@@ -1,9 +0,0 @@
-class AppError extends Error {
-    constructor(statusCode, status, message) {
-        super(message);
-        this.statusCode = statusCode;
-        this.status = status;
-    }
-}
-
-export default AppError;
