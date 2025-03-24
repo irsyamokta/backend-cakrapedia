105260d5f9d0595a5492c20468d3e59edab6dd0a
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-24 23:00:07 +0700

    Update : User controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,12 @@
 import prisma from "../config/db.js";
+import crypto from "crypto";
+import { sendVerificationEmail } from "../utils/Email.js";
 
 export const getProfile = async (req, res) => {
     try {
         const user = await prisma.user.findUnique({
             where: { id: req.user.userId },
-            select: { id: true, name: true, email: true, role: true }
+            select: { id: true, name: true, email: true, birthDate: true, gender: true, role: true }
         });
 
         if (!user) {
@@ -16,7 +18,12 @@ export const getProfile = async (req, res) => {
             );
         }
 
-        res.json(user);
+        res.json(
+            {
+                status: "success",
+                data: user
+            }
+        );
     } catch (error) {
         res.status(500).json(
             {
@@ -29,24 +36,49 @@ export const getProfile = async (req, res) => {
 
 export const updateProfile = async (req, res) => {
     try {
-        const { name, email } = req.body;
+        const { name, email, birthDate, gender } = req.body;
+        const userId = req.user.userId;
+
+        const existingUser = await prisma.user.findUnique({ where: { id: userId }, select: { email: true } });
+
+        if (!existingUser) {
+            return res.status(404).json(
+                {
+                    status: "fail",
+                    message: "User not found",
+                }
+            );
+        }
+
+        let verificationToken = null;
+        let isVerified = false;
+
+        if (email && email !== existingUser.email) {
+            verificationToken = crypto.randomBytes(32).toString("hex");
+            isVerified = false;
+
+            await sendVerificationEmail(name, email, verificationToken);
+        }
 
         const updatedUser = await prisma.user.update({
             where: { id: req.user.userId },
-            data: { name, email },
-            select: { id: true, name: true, email: true, role: true }
+            data: {
+                name, email, birthDate: new Date(birthDate), gender, isVerified,
+                verificationToken
+            },
+            select: { id: true, name: true, email: true, birthDate: true, gender: true, role: true },
         });
 
         res.json({
             status: "success",
-            message: "User profile updated successfully",
+            message: email !== existingUser.email ? "Verification email sent" : "User profile updated successfully",
             data: updatedUser
         });
     } catch (error) {
         res.status(500).json(
             {
                 status: "error",
-                error: "Error updating user profile"
+                error: error.message || "Error updating user profile"
             }
         );
     }
