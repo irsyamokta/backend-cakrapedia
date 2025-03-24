a60343cc123b8ef4eac9615eb6b37ac43a25bb6a
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-24 23:00:50 +0700

    Add : Middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getProfile, updateProfile } from "../controllers/userController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { checkVerifiedUser } from "../middlewares/checkVerifiedUser.js";
 
 const router = express.Router();
 
-router.get("/profile", authMiddleware, getProfile);
-router.put("/update", authMiddleware, updateProfile);
+router.get("/profile", authMiddleware, checkVerifiedUser, getProfile);
+router.put("/update", authMiddleware, checkVerifiedUser, updateProfile);
 
 export default router;
