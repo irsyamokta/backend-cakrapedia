5a6063cc70d9a26f7c4b5c6d586a40774be3f9c2
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-25 18:21:38 +0700

    Update : User controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,85 +1,27 @@
-import prisma from "../config/db.js";
-import crypto from "crypto";
-import { sendVerificationEmail } from "../utils/Email.js";
+import { getUserProfile, updateUserProfile } from "../services/userService.js";
+import { updateProfileValidator } from "../utils/validators/index.js";
+import { BadRequestError } from "../utils/errors/errors.js";
 
-export const getProfile = async (req, res) => {
+export const getProfile = async (req, res, next) => {
     try {
-        const user = await prisma.user.findUnique({
-            where: { id: req.user.userId },
-            select: { id: true, name: true, email: true, birthDate: true, gender: true, role: true }
-        });
-
-        if (!user) {
-            return res.status(404).json(
-                {
-                    status: "fail",
-                    message: "Akun tidak ditemukan",
-                }
-            );
-        }
-
-        res.json(
-            {
-                status: "success",
-                data: user
-            }
-        );
+        const user = await getUserProfile(req.user.userId);
+        res.json({ status: "success", data: user });
     } catch (error) {
-        res.status(500).json(
-            {
-                status: "error",
-                error: "Terjadi kesalahan pada server"
-            }
-        );
+        next(error);
     }
 };
 
-export const updateProfile = async (req, res) => {
+export const updateProfile = async (req, res, next) => {
     try {
-        const { name, email, birthDate, gender } = req.body;
-        const userId = req.user.userId;
-
-        const existingUser = await prisma.user.findUnique({ where: { id: userId }, select: { email: true } });
-
-        if (!existingUser) {
-            return res.status(404).json(
-                {
-                    status: "fail",
-                    message: "Akun tidak ditemukan",
-                }
-            );
+        const { error } = updateProfileValidator(req.body);
+        if (error) {
+            const messages = error.details.map(err => err.message);
+            throw new BadRequestError("Validasi gagal", messages); 
         }
 
-        let verificationToken = null;
-        let isVerified = false;
-
-        if (email && email !== existingUser.email) {
-            verificationToken = crypto.randomBytes(32).toString("hex");
-            isVerified = false;
-
-            await sendVerificationEmail(name, email, verificationToken);
-        }
-
-        const updatedUser = await prisma.user.update({
-            where: { id: req.user.userId },
-            data: {
-                name, email, birthDate: new Date(birthDate), gender, isVerified,
-                verificationToken
-            },
-            select: { id: true, name: true, email: true, birthDate: true, gender: true, role: true },
-        });
-
-        res.json({
-            status: "success",
-            message: email !== existingUser.email ? "Email verifikasi telah dikirim" : "User profile berhasil diperbarui",
-            data: updatedUser
-        });
+        const { updatedUser, message } = await updateUserProfile(req.user.userId, req.body);
+        res.json({ status: "success", message, data: updatedUser });
     } catch (error) {
-        res.status(500).json(
-            {
-                status: "error",
-                error: "Terjadi kesalahan pada server"
-            }
-        );
+        next(error);
     }
 };
