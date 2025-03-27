16e3767b7a6341d266f657b055a0e1e00dedf6cb
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-27 22:33:45 +0700

    Update : Admin routes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import  { isAdmin }  from "../middlewares/adminMiddleware.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { dashboard } from "../services/adminService.js";
+import { getDashboard, reviewRequest } from "../controllers/adminController.js";
 
 const router = express.Router();
 
-router.get("/dashboard", authMiddleware, isAdmin, dashboard)
+router.get("/dashboard", authMiddleware, isAdmin, getDashboard)
+router.put("/review-request", authMiddleware, isAdmin, reviewRequest)
 
 export default router;
