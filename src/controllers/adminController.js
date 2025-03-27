c5b831f5bb7e316f378abfe463e7e0d3fafe56a0
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-27 22:31:20 +0700

    Update : Admin controller

---
+++ src/controllers/adminController.js
@@ -0,0 +1,20 @@
+import * as adminService from "../services/adminService.js";
+
+export const getDashboard = async (req, res, next) => {
+    try {
+        const response = await adminService.dashboard();
+        return res.json({ status: "success", ...response });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const reviewRequest = async (req, res, next) => {
+    try {
+        const response = await adminService.reviewRoleRequest(req.user.userId, req.body);
+        return res.json({ status: "success", ...response });
+    } catch (error) {
+        next(error);
+    }
+};
+
