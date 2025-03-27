4cea79d81c201e19d36723c71efe6ea8b896b0dd
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-27 22:31:41 +0700

    Update : User controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
-import { getUserProfile, updateUserProfile } from "../services/userService.js";
-import { updateProfileValidator } from "../utils/validators/index.js";
+import { getUserProfile, updateUserProfile, requestRoleChange } from "../services/userService.js";
+import { updateProfileValidator, requestRoleValidator } from "../utils/validators/index.js";
 import { BadRequestError } from "../utils/errors/errors.js";
 
 export const getProfile = async (req, res, next) => {
@@ -16,11 +16,27 @@ export const updateProfile = async (req, res, next) => {
         const { error } = updateProfileValidator(req.body);
         if (error) {
             const messages = error.details.map(err => err.message);
-            throw new BadRequestError("Validasi gagal", messages); 
+            throw new BadRequestError("Validasi gagal", messages);
         }
 
         const { updatedUser, message } = await updateUserProfile(req.user.userId, req.body);
         res.json({ status: "success", message, data: updatedUser });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const requestRole = async (req, res, next) => {
+    try {
+        const { error } = requestRoleValidator(req.body);
+        
+        if (error) {
+            const messages = error.details.map(err => err.message);
+            throw new BadRequestError("Validasi gagal", messages);
+        }
+
+        const { requestedRole, message } = await requestRoleChange(req.user.userId, req.body);
+        res.json({ status: "success", message, data: requestedRole });  
     } catch (error) {
         next(error);
     }
