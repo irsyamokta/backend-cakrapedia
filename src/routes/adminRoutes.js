9e2926fa310d175bc888c7dac7664515abe8d784
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-28 16:47:44 +0700

    Update : Path module

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import  { isAdmin }  from "../middlewares/adminMiddleware.js";
+import  { isAdmin }  from "../middlewares/roleMiddleware.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { getDashboard, reviewRequest } from "../controllers/adminController.js";
 
