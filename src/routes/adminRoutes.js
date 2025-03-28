11c8d3249d9640beddf2601c50ee863c22f58551
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-28 22:31:00 +0700

    Update : New routes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import  { isAdmin }  from "../middlewares/roleMiddleware.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { getDashboard, reviewRequest } from "../controllers/adminController.js";
+import { getDashboard, reviewRequest, createCategory } from "../controllers/adminController.js";
 
 const router = express.Router();
 
-router.get("/dashboard", authMiddleware, isAdmin, getDashboard)
-router.put("/review-request", authMiddleware, isAdmin, reviewRequest)
+router.get("/dashboard", authMiddleware, isAdmin, getDashboard);
+router.put("/review-request", authMiddleware, isAdmin, reviewRequest);
+router.post("/create-category", authMiddleware, isAdmin, createCategory);
 
 export default router;
