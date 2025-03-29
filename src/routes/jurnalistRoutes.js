b94a48993916b5412cc4ecb9ad1feba89a0cb609
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-29 18:40:09 +0700

    Update : Jurnalist routes

--- src/routes/jurnalistRoutes.js
+++ src/routes/jurnalistRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import  { isJurnalist }  from "../middlewares/roleMiddleware.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { createNews } from "../controllers/jurnalistController.js";
+import { getNews,createNews, updateNews, deleteNews } from "../controllers/jurnalistController.js";
 
 const router = express.Router();
 
-router.get("/dashboard", authMiddleware, isJurnalist);
+router.get("/dashboard", authMiddleware, isJurnalist, getNews);
 router.post("/create-news", authMiddleware, isJurnalist, createNews);
+router.put("/update-news/:newsId", authMiddleware, isJurnalist, updateNews);
+router.delete("/delete-news/:newsId", authMiddleware, isJurnalist, deleteNews);
 
 export default router;
