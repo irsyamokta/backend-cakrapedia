76a86d388581d3e8d8dcb86f27ae8bc46801c32c
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-03-30 16:43:43 +0700

    Update : Jurnalist service

--- src/services/jurnalistService.js
+++ src/services/jurnalistService.js
@@ -1,5 +1,6 @@
 import prisma from "../config/db.js";
 import { createNewsValidator, updateNewsValidator } from "../utils/validators/index.js";
+import { uploadImage } from "./uploadService.js";
 import { BadRequestError, NotFoundError } from "../utils/errors/errors.js";
 
 export const getNews = async (userId) => {
@@ -11,7 +12,11 @@ export const getNews = async (userId) => {
     return { news };
 }
 
-export const createNews = async (data) => {
+export const createNews = async (userId,data, file) => {
+    if (!file) {
+        throw new BadRequestError("Gambar tidak boleh kosong!", ["Upload gamabar diperlukan"]);
+    }
+
     const { error } = createNewsValidator(data);
     if (error) {
         const messages = error.details.map(err => err.message);
@@ -19,14 +24,16 @@ export const createNews = async (data) => {
     }
 
     const { title, content, categoryId } = data;
-
-    const author = await prisma.user.findFirst({ where: { role: "JURNALIS" }, select: { id: true, name: true } });
+    const imageUrl = await uploadImage(file, "news");
+    
+    const author = await prisma.user.findUnique({ where: { id: userId, role: "JURNALIS" }, select: { id: true, name: true } });
     if (!author) throw new NotFoundError("Penulis tidak ditemukan");
 
     const news = await prisma.news.create({
         data: {
             title,
             content,
+            imageUrl: imageUrl.fileUrl,
             categoryId,
             authorId: author.id
         },
@@ -35,7 +42,11 @@ export const createNews = async (data) => {
     return { message: "Berita berhasil dibuat", news };
 };
 
-export const updateNews = async (userId, newsId, data) => {
+export const updateNews = async (userId, newsId, data, file) => {
+    if (!file) {
+        throw new BadRequestError("Gambar tidak boleh kosong!", ["Upload gamabar diperlukan"]);
+    }
+
     const { error } = updateNewsValidator(data);
     if (error) {
         const messages = error.details.map(err => err.message);
@@ -43,20 +54,22 @@ export const updateNews = async (userId, newsId, data) => {
     }
 
     const { title, content, categoryId } = data;
+    const imageUrl = await uploadImage(file, "news");
 
     const user = await prisma.user.findUnique({ where: { id: userId } });
     if (!user) throw new NotFoundError("Akun tidak ditemukan");
-
+    
     const news = await prisma.news.findUnique({ where: { id: newsId } });
     if (!news) throw new NotFoundError("Berita tidak ditemukan");
-
+    
     if (news.authorId !== user.id) throw new BadRequestError("Anda tidak memiliki izin untuk mengubah berita ini", ["Anda bukan penulis berita ini"]);
-
+    
     const updatedNews = await prisma.news.update({
         where: { id: newsId },
         data: {
             title: title || news.title,
             content: content || news.content,
+            imageUrl: imageUrl.fileUrl || news.imageUrl,
             categoryId: categoryId ?? news.categoryId
         },
     });
