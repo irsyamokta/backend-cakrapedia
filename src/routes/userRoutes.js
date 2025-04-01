5fd91f14a299c70f1b5967648bb9a584ffa37718
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-01 20:34:39 +0700

    Update : User routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { getProfile, updateProfile, requestRole } from "../controllers/userController.js";
+import { multerUpload } from "../config/multer.js";
+import { getProfile, updateProfile, requestRole, deleteUser } from "../controllers/userController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { checkVerifiedUser } from "../middlewares/checkVerifiedUser.js";
 
 const router = express.Router();
 
 router.get("/profile", authMiddleware, checkVerifiedUser, getProfile);
-router.put("/update", authMiddleware, checkVerifiedUser, updateProfile);
-router.post("/request-role", authMiddleware, checkVerifiedUser, requestRole);
+router.put("/update", authMiddleware, checkVerifiedUser, multerUpload, updateProfile);
+router.post("/request-role", authMiddleware, checkVerifiedUser, multerUpload, requestRole);
+router.delete("/delete-account", authMiddleware, checkVerifiedUser, deleteUser);
 
 export default router;
