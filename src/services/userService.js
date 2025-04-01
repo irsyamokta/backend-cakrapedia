ba2182978d8018eefd72ef97a4963b564fb7e7bd
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-01 20:33:33 +0700

    Update : User service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,12 +1,14 @@
 import prisma from "../config/db.js";
 import crypto from "crypto";
+import { uploadImage, uploadPDF } from "./uploadService.js";
 import { sendVerificationEmail } from "../utils/email/index.js";
 import { NotFoundError, BadRequestError } from "../utils/errors/errors.js";
+import { updateProfileValidator, requestRoleValidator } from "../utils/validators/index.js";
 
 export const getUserProfile = async (userId) => {
     const user = await prisma.user.findUnique({
         where: { id: userId },
-        select: { id: true, name: true, email: true, birthDate: true, gender: true, role: true }
+        select: { id: true, name: true, email: true, birthDate: true, gender: true, role: true, imageUrl: true },
     });
 
     if (!user) throw new NotFoundError("Akun tidak ditemukan");
@@ -14,40 +16,48 @@ export const getUserProfile = async (userId) => {
     return user;
 };
 
-export const updateUserProfile = async (userId, { name, email, birthDate, gender }) => {
-    const existingUser = await prisma.user.findUnique({ where: { id: userId }, select: { email: true } });
-    if (!existingUser) throw new NotFoundError("Akun tidak ditemukan");
+export const updateUserProfile = async (userId, data, file) => {
+    const { error } = updateProfileValidator(data);
+    if (error) throw new BadRequestError("Validasi gagal", error.details.map(err => err.message));
 
-    let verificationToken = null;
-    let isVerified = true;
+    const user = await prisma.user.findUnique({ where: { id: userId }, select: { email: true } });
+    if (!user) throw new NotFoundError("Akun tidak ditemukan");
+
+    const { name, email, birthDate, gender } = data;
+    const imageUrl = await uploadImage(file, "profile");    
+
+    let updateData = { name, birthDate: birthDate ? new Date(birthDate) : null, gender, imageUrl: imageUrl.fileUrl };
     let message = "User profile berhasil diperbarui";
 
-    if (email && email !== existingUser.email) {
-        verificationToken = crypto.randomBytes(32).toString("hex");
-        isVerified = false;
-        await sendVerificationEmail(name, email, verificationToken);
+    if (email && email !== user.email) {
+        const emailExists = await prisma.user.findUnique({ where: { email } });
+        if (emailExists) throw new BadRequestError("Email sudah digunakan!", ["Email sudah terdaftar"]);
+
+        updateData.isVerified = false;
+        updateData.verificationToken = crypto.randomBytes(32).toString("hex");
+
+        await sendVerificationEmail(name, email, updateData.verificationToken);
         message = "Email verifikasi telah dikirim";
     }
 
     const updatedUser = await prisma.user.update({
         where: { id: userId },
-        data: {
-            name,
-            email,
-            birthDate: birthDate ? new Date(birthDate) : null,
-            gender,
-            isVerified,
-            verificationToken
-        },
-        select: { id: true, name: true, email: true, birthDate: true, gender: true, role: true },
+        data: updateData,
+        select: { id: true, name: true, email: true, birthDate: true, gender: true, role: true, imageUrl: true },
     });
 
     return { updatedUser, message };
 };
 
-export const requestRoleChange = async (userId, data) => {
+export const requestRoleChange = async (userId, data, file) => {
+    const { error } = requestRoleValidator(data);
+    if (error) {
+        const messages = error.details.map(err => err.message);
+        throw new BadRequestError("Validasi gagal", messages);
+    }
 
-    const { roleRequested, portfolio } = data;
+    const { roleRequested } = data;
+    const pdfUrl = await uploadPDF(file);
 
     const user = await prisma.user.findUnique({ where: { id: userId } });
     if (!user) throw new NotFoundError("Akun tidak ditemukan");
@@ -61,7 +71,17 @@ export const requestRoleChange = async (userId, data) => {
 
     if (existingRequest) throw new BadRequestError("Tidak dapat membuat permintaan baru", ["Permintaan sudah dikirim sebelumnya"]);
 
-    const userRequest = await prisma.userRequest.create({ data: { userId, roleRequested, portfolio }, });
+    const userRequest = await prisma.userRequest.create({ data: { userId, roleRequested, portfolio: pdfUrl.fileUrl }, });
 
     return { message: "Permintan berhasil dikirim", userRequest };
+};
+
+export const deleteUser = async (userId) => {
+    const user = await prisma.user.findUnique({ where: { id: userId } });    
+    if (!user) throw new NotFoundError("Akun tidak ditemukan");
+
+    if(user.role === "ADMIN" || user.role === "JURNALIS" || user.role === "EDITOR") throw new BadRequestError("Anda tidak memiliki izin untuk menghapus akun ini", ["Tidak dapat menghapus akun"]);
+    
+    await prisma.user.delete({ where: { id: userId } });    
+    return { message: "Akun berhasil dihapus" };    
 };
