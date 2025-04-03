78a0b6f340b4e3d4b58da565785d43ec4b98ae5e
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-03 23:58:55 +0700

    Refactor : Password controller

---
+++ src/controllers/password.controller.js
@@ -0,0 +1,28 @@
+import * as passwordService from "../services/password.service.js"
+
+export const changeUserPassword = async (req, res, next) => {
+    try {
+        const result = await passwordService.changeUserPassword(req.user.userId, req.body);
+        res.status(200).json({ status: "success", ...result });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const forgotUserPassword = async (req, res, next) => {
+    try {
+        const result = await passwordService.forgotUserPassword(req.body);
+        res.status(200).json({ status: "success", ...result });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const resetUserPassword = async (req, res, next) => {
+    try {
+        const result = await passwordService.resetUserPassword(req.body);
+        res.status(200).json({ status: "success", ...result })
+    } catch (error) {
+        next(error);
+    }
+};
