12c7fa4fc521eb454ce6e28079d2d41e624c1224
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-04 22:20:42 +0700

    Update : User repository

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,5 +1,21 @@
 import prisma from "../config/db.js";
 
+export const getUsers = async (role) => {
+    return prisma.user.findMany({
+        where: { role: `${role}` },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            role: true,
+            status: true,
+            birthDate: true,
+            gender: true,
+            imageUrl: true
+        }
+    });
+};
+
 export const getUserById = async (userId, selectedField = null) => {
     return prisma.user.findUnique({
         where: { id: userId },
@@ -22,21 +38,15 @@ export const updateUserprofile = async (userId, data) => {
     });
 };
 
-export const getExistingRequest = async (userId, status) => {
-    return prisma.userRequest.findFirst({
-        where: { id: userId, status: status }
-    });
-};
-
-export const createUserRequestRole = async (userId, data, file) => {
-    return prisma.userRequest.create({
+export const updateUserRole = async (userId, roleRequested, action) => {
+    return prisma.user.update({
+        where: { id: userId },
         data: {
-            userId: userId,
-            roleRequested: data,
-            portfolio: file
+            role: roleRequested,
+            status: action
         }
-    })
-};
+    });
+}
 
 export const deleteUser = async (userId) => {
     return prisma.user.delete({
