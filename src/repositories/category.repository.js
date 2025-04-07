0953072728459be209ca047855c04bb9bd8db46e
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-07 22:53:21 +0700

    Update : Category repository

--- src/repositories/category.repository.js
+++ src/repositories/category.repository.js
@@ -4,6 +4,10 @@ export const getCategory = async (name) => {
     return prisma.category.findUnique({ where: { name: name } })
 };
 
+export const getCategoryById = async (categoryId) => {
+    return prisma.category.findUnique({ where: { id: categoryId } });
+};
+
 export const createCategory = async (name) => {
     return prisma.category.create({ data: { name: name } });
 };
