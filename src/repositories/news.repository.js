7fba3af97cabb71f639d768a667244c20bb45a45
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-09 23:42:50 +0700

    Update : News repository

--- src/repositories/news.repository.js
+++ src/repositories/news.repository.js
@@ -3,7 +3,9 @@ import prisma from "../config/db.js";
 
 export const getAuthor = async (userId) => prisma.user.findUnique({ where: { id: userId, role: { in: ["JURNALIS", "EDITOR"] } }, select: { id: true } });
 
-export const createNewsData = async (title, content, categoryId, imageUrl, userId) => {
+export const getNewsById = async(newsId) => prisma.news.findUnique({ where: { id: newsId } });
+
+export const createNews = async (title, content, categoryId, imageUrl, userId) => {
     return prisma.news.create({
         data: {
             title,
@@ -12,5 +14,12 @@ export const createNewsData = async (title, content, categoryId, imageUrl, userI
             categoryId,
             authorId: userId
         }
+    });
+};
+
+export const updateNews = async (newsId, data) => {
+    return prisma.news.update({ 
+        where: { id: newsId }, 
+        data
     });
 };
