4ce3c0720c4f449f972de327f1b2506d67768269
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-11 23:07:49 +0700

    Update : News routes

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { multerUpload } from "../config/multer.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { createNews, updateNews } from "../controllers/news.controller.js";
+import { createNews, updateNews, deleteNews } from "../controllers/news.controller.js";
 import { hasRole } from "../middlewares/roleMiddleware.js"; 
 
 const router = express.Router();
@@ -10,5 +10,6 @@ const isAllowed = hasRole("JURNALIS", "EDITOR", "ADMIN");
 
 router.post("/create", authMiddleware, isAllowed, multerUpload, createNews);
 router.put("/update/:newsId", authMiddleware, isAllowed, multerUpload, updateNews);
+router.delete("/delete/:newsId", authMiddleware, isAllowed, deleteNews);
 
 export default router;
