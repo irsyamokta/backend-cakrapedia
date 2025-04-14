5a388fef2b00e12f439e57296fb9e6a42fc1aba3
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-14 19:58:30 +0700

    Update : News routes

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { multerUpload } from "../config/multer.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { createNews, updateNews, deleteNews } from "../controllers/news.controller.js";
+import { getNews, getNewsById, getNewsByAuthor, getNewsByCategory, getNewsPublished, createNews, updateNews, deleteNews } from "../controllers/news.controller.js";
 import { hasRole } from "../middlewares/roleMiddleware.js"; 
 
 const router = express.Router();
@@ -11,5 +11,11 @@ const isAllowed = hasRole("JURNALIS", "EDITOR", "ADMIN");
 router.post("/create", authMiddleware, isAllowed, multerUpload, createNews);
 router.put("/update/:newsId", authMiddleware, isAllowed, multerUpload, updateNews);
 router.delete("/delete/:newsId", authMiddleware, isAllowed, deleteNews);
+
+router.get("/view", authMiddleware, hasRole("ADMIN", "EDITOR"), getNews);
+router.get("/view/:newsId", authMiddleware, hasRole("ADMIN", "EDITOR"), getNewsById);
+router.get("/category/:categoryId", authMiddleware, getNewsByCategory);
+router.get("/author", authMiddleware, isAllowed, getNewsByAuthor);
+router.get("/published", getNewsPublished);
 
 export default router;
