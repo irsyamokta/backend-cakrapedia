2f2d9f1ec6a79c7a32dc232537f6a1cd6e7ea60f
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-25 19:32:43 +0700

    Add : Verified middleware

---
+++ src/middlewares/verified.middleware.js
@@ -0,0 +1,37 @@
+import prisma from "../config/db.js";
+
+export const checkVerifiedUser = async (req, res, next) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id: req.user.id },
+            select: { isVerified: true }
+        });
+
+        if (!user) {
+            return res.status(404).json(
+                {
+                    status: "fail",
+                    message: "Akun tidak ditemukan"
+                }
+            );
+        }
+
+        if (!user.isVerified) {
+            return res.status(403).json(
+                {
+                    status: "forbidden",
+                    message: "Email tidak diverifikasi. Silakan verifikasi email terlebih dahulu."
+                }
+            );
+        }
+
+        next();
+    } catch (error) {
+        res.status(500).json(
+            {
+                status: "error",
+                message: "Terjadi kesalahan pada server"
+            }
+        );
+    }
+};
