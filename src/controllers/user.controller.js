bc86ba4972e87179ebeda32959ef8e2ee91344be
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-26 23:34:03 +0700

    Update : User controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,7 +20,7 @@ export const getUserProfile = async (req, res, next) => {
 
 export const updateUserProfile = async (req, res, next) => {
     try {
-        const { result, message } = await userService.updateUserProfile(req.user.userId, req.body, req.file);
+        const { result, message } = await userService.updateUserProfile(req.user.id, req.body, req.file);
         res.status(200).json({ message, data: result });
     } catch (error) {
         next(error);
@@ -38,7 +38,7 @@ export const requestRoleChange = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
     try {
-        const result = await userService.deleteUser(req.user.userId);
+        const result = await userService.deleteUser(req.user.id);
         res.status(200).json(result);
     } catch (error) {
         next(error);
