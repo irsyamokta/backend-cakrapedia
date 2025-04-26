41412d535da76de8b18a1fb36ef4be64eaea3c99
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-26 23:33:21 +0700

    Update : Admin controller

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -20,3 +20,30 @@ export const createCategory = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getCategories = async (req, res, next) => {
+    try {
+        const result = await adminService.getCategories();
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteCategory = async (req, res, next) => {
+    try {
+        const result = await adminService.deleteCategory(req.params.categoryId);
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteUserById = async (req, res, next) => {
+    try {
+        const result = await adminService.deleteUserById(req.params.userId, req.user.id);
+        res.status(200).json({ status: "success", ...result });
+    } catch (error) {
+        next(error);
+    }
+};
