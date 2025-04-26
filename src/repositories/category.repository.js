5f44eca38e3a38ac2739ed29624734303aa78e9e
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-04-26 23:34:40 +0700

    Update : Category repository

--- src/repositories/category.repository.js
+++ src/repositories/category.repository.js
@@ -1,7 +1,11 @@
 import prisma from "../config/db.js";
 
-export const getCategory = async (name) => {
-    return prisma.category.findUnique({ where: { name: name } })
+export const getCategories = async () => {
+    return prisma.category.findMany();
+};
+
+export const getCategoriesByName = async (name) => {
+    return prisma.category.findUnique({ where: { name: name } });
 };
 
 export const getCategoryById = async (categoryId) => {
@@ -11,3 +15,7 @@ export const getCategoryById = async (categoryId) => {
 export const createCategory = async (name) => {
     return prisma.category.create({ data: { name: name } });
 };
+
+export const deleteCategory = async (categoryId) => {
+    return prisma.category.delete({ where: { id: categoryId } });
+}
