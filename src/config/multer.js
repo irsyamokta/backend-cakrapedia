81523ee85609e02185c6e736365f65b9f448f39a
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-11 21:08:51 +0700

    Update : Multer config

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,13 +1,6 @@
-import { Storage } from "@google-cloud/storage"; 
 import multer from "multer";
 
-const projectId = process.env.PROJECT_ID;
-const bucketName = process.env.BUCKET_NAME;
+const multerStorage = multer.memoryStorage();
+const multerUpload = multer({ storage: multerStorage }).single("file");
 
-const storage = new Storage({ projectId });
-const bucket = storage.bucket(bucketName);
-
-const multerStorage = multer.memoryStorage(); 
-const multerUpload = multer({ storage: multerStorage }).single("file"); 
-
-export { multerUpload, bucket };
+export { multerUpload };
