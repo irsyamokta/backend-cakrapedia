9a46546ff09f0daf4339900fc229882e33a72904
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-11 21:08:20 +0700

    Update : Auth repository

--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -2,16 +2,7 @@ import prisma from "../config/db.js";
 
 export const getUserByEmail = async (email) => prisma.user.findUnique({ where: { email } });
 
-export const createUser = async (data) => prisma.user.create({
-    data: {
-        name: data.name,
-        email: data.email,
-        password: data.password,
-        birthDate: new Date(data.birthDate),
-        gender: data.gender,
-        verificationToken: data.verificationToken
-    }
-});
+export const createUser = async (data) => prisma.user.create({ data });
 
 export const updateUserRefreshToken = async (userId, refreshToken) => prisma.user.update({ where: { id: userId }, data: { refreshToken } });
 
