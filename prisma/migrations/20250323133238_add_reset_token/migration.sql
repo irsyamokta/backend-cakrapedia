7ac74a0a537fadbc3546a0638bbe0120ebf0456d
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-11 21:09:47 +0700

    Del : Unused migrations

--- prisma/migrations/20250323133238_add_reset_token/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `resetExpires` DATETIME(3) NULL,
-    ADD COLUMN `resetToken` VARCHAR(191) NULL;
--- prisma/migrations/20250427130638_add_reject_reason_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `news` ADD COLUMN `rejectReason` VARCHAR(191) NULL;
-
--- AlterTable
-ALTER TABLE `userrequest` ADD COLUMN `rejectReason` VARCHAR(191) NULL;
