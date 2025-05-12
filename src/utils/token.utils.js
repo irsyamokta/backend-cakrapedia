90ac87a77088c0d56fdae69cd283fc44125e3dfc
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-12 23:55:08 +0700

    Update : Token utils

--- src/utils/token.utils.js
+++ src/utils/token.utils.js
@@ -30,5 +30,4 @@ export const cookieOptions = () => ({
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "Strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 });
