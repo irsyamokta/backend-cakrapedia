52f9759754e5f1100a525764d83548e8d872efe9
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-13 17:38:31 +0700

    Update : Password controller

--- src/controllers/password.controller.js
+++ src/controllers/password.controller.js
@@ -20,8 +20,8 @@ export const forgotUserPassword = async (req, res, next) => {
 
 export const resetUserPassword = async (req, res, next) => {
     try {
-        const result = await passwordService.resetUserPassword(req.body);
-        res.status(200).json({ status: "success", ...result })
+        await passwordService.resetUserPassword(req.params.token, req.body);
+        res.status(200).json({ status: "success" });
     } catch (error) {
         next(error);
     }
