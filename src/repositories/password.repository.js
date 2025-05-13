493b3ebf28ef68dc52eb96dee5de7f34faaf087d
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-05-13 17:38:43 +0700

    Update : Password repository

--- src/repositories/password.repository.js
+++ src/repositories/password.repository.js
@@ -1,5 +1,5 @@
 import prisma from "../config/db.js";
 
-export const updateUserPassword = async (userId, password) => prisma.user.update({ where: { id: userId }, data: { password, resetToken: null, resetExpires: null } });
+export const updateUserPassword = async (userId, data) => prisma.user.update({ where: { id: userId }, data });
 
 export const updateUserResetToken = async (userId, resetToken, resetExpires) => prisma.user.update({ where: { id: userId }, data: { resetToken, resetExpires } });
